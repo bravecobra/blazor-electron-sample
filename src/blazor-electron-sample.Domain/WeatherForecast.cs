d9ce7a9e98faa80ed587b8959f6440df78e3e269
Brave Cobra <bravecobra@gmail.com>
2023-01-22 19:26:35 +0100

    Update package.json

--- src/blazor-electron-sample.Domain/WeatherForecast.cs
+++ src/blazor-electron-sample.Domain/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace blazor_electron_sample.Domain
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string Summary { get; set; } = null!;
     }
 }
